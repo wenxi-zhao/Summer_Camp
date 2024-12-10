59a888f0e104c39d76607d4db4c6734191000e90
geralt-ji <qq547608233@gmail.com>
2024-12-10 17:53:40 +0800

    添加 GitHub Pages 部署配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    baseURL: '/Summer_Camp/', // 仓库名称
+    buildAssetsDir: 'assets', // 不要使用_nuxt
+  },
 })
