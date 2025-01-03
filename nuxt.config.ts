a7ae010f72ad0483f646e9760d8a310ec3f438a2
赵文希 <124047665+wenxi-zhao@users.noreply.github.com>
2025-01-03 20:56:59 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   app: {
-    baseURL: '/Summer_Camp/', // 仓库名称
+    baseURL: '/', // 仓库名称
     buildAssetsDir: 'assets', // 不要使用_nuxt
   },
   plugins: [
